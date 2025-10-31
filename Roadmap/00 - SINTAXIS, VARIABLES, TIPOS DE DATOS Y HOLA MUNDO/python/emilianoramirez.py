e1a6e3c88c2ee84c618224602ffef1246399d4c0
emilianoramirez-droid <brayantijerinramirez@gmail.com>
2025-10-31 02:07:55 -0600

    Create emilianoramirez.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/emilianoramirez.py
@@ -0,0 +1,25 @@
+https://www.python.org/
+# comentario en lineas
+""""
+Comentario de varias lineas
+""""
+''''
+tambien es
+un co9mentario de varias 
+lineas
+''''
+# Variable
+hola_sss = "hola" 
+# phyton no tiene constantes
+
+# Datos primitivos
+mi_int = 2
+mi_float = 1.23 
+mi_boleano = true
+mi_boleano= false
+mi_string = "cadena"
+mi_otro_string= "otra cadena"
+
+imprimir("hola, phyton")
+
+
